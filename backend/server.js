7211943363ea70750cd0a32cbfa09999966bb119
maulik-sharma <maulik.jaipur@gmail.com>
2026-05-03 23:43:33 +0530

    Added trust proxy

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const { userApiLimiter } = require("./middleware/rateLimiters");
 
 const app = express();
 expressWs(app);   // patches app with app.ws() support
-
+app.set("trust proxy", true);
 app.use(express.json());
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:3000",
